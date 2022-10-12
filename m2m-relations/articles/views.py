41609f8e18ef81c6c6bd71ca3618b6224d01b959
Muxriddin <muxri1334@mail.ru>
2022-10-12 19:17:52 +0300

    Changed file m2m-relations/articles/views.py

--- m2m-relations/articles/views.py
+++ m2m-relations/articles/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Article
 
 def articles_list(request):
-    print(object_list:=Article.objects.all())
+    object_list = Article.objects.all()
     template = 'articles/news.html'
     context = {'object_list': object_list}
 
